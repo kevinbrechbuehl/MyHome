deb848116d78ee7c63dd47dea986e04cc587ddd4
Kevin Brechbühl <mail@aquasonic.ch>
2020-06-21 21:48:51 +0200

    Configure Netlify Functions

---
+++ netlify-lambda.js
@@ -0,0 +1,50 @@
+// Local plugin to integrate netlify-lambda serve and build into Vue CLI serve and build command.
+// Original source: https://github.com/netlify/vue-cli-plugin-netlify-lambda/blob/master/index.js
+
+const { fork } = require("child_process");
+const lambdaBuild = require("netlify-lambda/lib/build");
+
+module.exports = (api, projectOptions) => {
+  const { build, serve } = api.service.commands;
+  const buildFn = build.fn;
+  const serveFn = serve.fn;
+  const webpackConfig =
+    projectOptions.pluginOptions &&
+    projectOptions.pluginOptions.netlify &&
+    projectOptions.pluginOptions.netlify.webpackConfig;
+
+  build.fn = async (...args) => {
+    try {
+      const [res, stats] = await Promise.all([
+        buildFn(...args),
+        lambdaBuild.run("src/lambda", webpackConfig)
+      ]);
+      console.log(stats.toString({ color: true }));
+      return res;
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  };
+
+  serve.fn = (...args) => {
+    const devServer = (api.service.projectOptions.devServer =
+      api.service.projectOptions.devServer || {});
+    if (!devServer.proxy) {
+      devServer.proxy = {};
+    }
+    devServer.proxy["/.netlify/functions"] = {
+      target: "http://localhost:9000",
+      pathRewrite: {
+        "^/\\.netlify/functions": ""
+      }
+    };
+
+    fork(require.resolve("netlify-lambda"), [
+      "serve",
+      "src/lambda",
+      ...(webpackConfig ? ["-c", webpackConfig] : [])
+    ]);
+    return serveFn(...args);
+  };
+};

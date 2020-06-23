4f8e75df71a21e795423a6fc6a79cfcded9f4163
Kevin Brechbühl <mail@aquasonic.ch>
2020-06-23 21:05:26 +0200

    Create overview view

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Overview from "../views/Overview.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
+    name: "Overview",
+    component: Overview
+  },
+  {
+    path: "/home",
     name: "Home",
     component: Home
   },

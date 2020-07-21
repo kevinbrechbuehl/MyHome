d2cb4237beddf366d21ecd8a89547c9a20446c7d
Kevin Brechbühl <mail@aquasonic.ch>
2020-07-21 21:42:57 +0200

    Add health index info for humidity and co2

---
+++ src/components/HealthIndex.vue
@@ -0,0 +1,76 @@
+<template>
+  <div
+    class="uk-flex uk-flex-middle text-xsmall uk-text-light margin-xsmall-top"
+  >
+    <span :class="'text-right icon ' + index"></span>
+    <span class="margin-xsmall-left">{{ title }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HealthIndex",
+  props: {
+    index: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    title() {
+      return this.index === "excellent"
+        ? "Excellent"
+        : this.index === "good"
+        ? "Good"
+        : this.index === "satisfying"
+        ? "Satisfying"
+        : this.index === "poor"
+        ? "Poor"
+        : this.index === "critical"
+        ? "Critical"
+        : null;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.uk-text-right .text-right {
+  margin-left: auto;
+}
+
+.text-xsmall {
+  font-size: 0.69rem;
+}
+
+.margin-xsmall-top {
+  margin-top: 5px;
+}
+
+.icon {
+  width: 8px;
+  height: 8px;
+  border-radius: 50%;
+  box-shadow: 0 0 1px 1px rgba(0, 0, 0, 0.08);
+}
+
+.excellent {
+  background-color: #82a3d9;
+}
+
+.good {
+  background-color: #31c79c;
+}
+
+.satisfying {
+  background-color: #fce800;
+}
+
+.poor {
+  background-color: #ffc46b;
+}
+
+.critical {
+  background-color: #ff6565;
+}
+</style>
--- src/components/IndoorModule.vue
+++ src/components/IndoorModule.vue
@@ -8,22 +8,61 @@
         >
       </div>
       <div class="uk-flex uk-flex-middle">
-        <span uk-icon="drop" ratio="0.8"></span>
-        <span class="margin-xsmall-left">{{ data.humidity }} %</span>
-        <span uk-icon="feather" ratio="0.4" class="flex-right"></span>
-        <span class="margin-xsmall-left">{{ data.co2 }} ppm</span>
+        <div>
+          <span uk-icon="drop" ratio="0.8"></span>
+          <span class="margin-xsmall-left"
+            >{{ data.humidity }} %<br /><HealthIndex :index="humidityIndex"
+          /></span>
+        </div>
+        <div class="flex-right">
+          <span uk-icon="feather" ratio="0.4"></span>
+          <span class="margin-xsmall-left"
+            >{{ data.co2 }} ppm<br /><HealthIndex
+              :index="co2Index"
+              class="uk-text-right"
+          /></span>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import HealthIndex from "@/components/HealthIndex.vue";
+
 export default {
   name: "IndoorModule",
+  components: {
+    HealthIndex
+  },
   props: {
     data: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    humidityIndex() {
+      return this.data.humidity >= 40 && this.data.humidity <= 50
+        ? "excellent"
+        : this.data.humidity >= 30 && this.data.humidity <= 60
+        ? "good"
+        : this.data.humidity >= 20 && this.data.humidity <= 70
+        ? "satisfying"
+        : this.data.humidity >= 15 && this.data.humidity <= 80
+        ? "poor"
+        : "critical";
+    },
+    co2Index() {
+      return this.data.co2 <= 900
+        ? "excellent"
+        : this.data.co2 <= 1150
+        ? "good"
+        : this.data.co2 <= 1400
+        ? "satisfying"
+        : this.data.co2 <= 1600
+        ? "poor"
+        : "critical";
     }
   }
 };
